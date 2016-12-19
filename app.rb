54503dbfef3ad4e584f263afa3508dba530863a7
Craig Day <cday@zendesk.com>
2016-12-19 13:53:34 -0600

    add some items and randomness

--- app.rb
+++ app.rb
@@ -3,18 +3,46 @@ require 'json'
 
 USER_MATCH = /(@\w+)/.freeze
 
+ITEMS = [
+  # fish
+  'a large trout',
+  'a trout',
+  'a halibut',
+
+  # slabs of meat
+  'a 32oz porterhouse',
+  'a slab of bacon',
+
+  # construction items
+  'a 2x4',
+  'a crowbar',
+  'a shovel'
+].freeze
+
 post '/' do
-  return 500 unless valid_token?
+  return 400 unless valid_token?
 
   response.headers['Content-Type'] = 'application/json'
 
+  message = [
+    "<@#{params['user_id']}|#{params['user_name']}",
+    'slaps',
+    params['text'],
+    'around a bit with',
+    random_item
+  ].join(' ')
+
   JSON.dump({
     response_type: "in_channel",
-    text: "<@#{params['user_id']}|#{params['user_name']}> slaps <#{params['text']}> around with a large trout"
+    text: message
   })
 end
 
 
 def valid_token?
   params['token'] == ENV['SLACK_TOKEN']
+end
+
+def random_item
+  ITEMS.sample
 end
